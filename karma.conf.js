1182921b314f001f9665c2147ba652393b11cd3f
Arnaud Méhat <amehat@cutivonsleweb.com>
2018-03-02 18:47:13 +0100

    add conf test

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,49 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    //basePath: '',
+    basePath: './',
     frameworks: ['jasmine', '@angular/cli'],
+    files: [
+      // paths loaded by Karma
+      //{pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
+      //{pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
+      //{pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
+      //{pattern: 'node_modules/angular2/bundles/angular2.dev.js', included: true, watched: true},
+      //{pattern: 'node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
+      //{pattern: 'node_modules/angular2/bundles/http.dev.js', included: true, watched: true},
+      //{pattern: 'karma-test-shim.js', included: true, watched: true},
+
+      // paths loaded via module imports
+      //{pattern: 'dist/**/*.js', included: true, watched: true},
+
+      {pattern: 'src/**/*.spec.ts', included: true, watched: false},
+
+      // paths to support debugging with source maps in dev tools
+      //{pattern: 'dist/**/*.js', included: false, watched: false},
+      //{pattern: 'dist/**/*.js.map', included: false, watched: false}
+    ],
+    // proxied base paths
+    proxies: {
+      // required for component assests fetched by Angular's compiler
+      //'/src/': '/base/src/'
+    },
     plugins: [
       require('karma-jasmine'),
+      require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
+    preprocessors: {
+      //'src/**/!(*spec).js': ['coverage'],
+      'src/app/**/!(*spec).ts': ['coverage']
+   },
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -22,7 +54,55 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: [
+      'progress', 
+      'kjhtml', 
+      'coverage',
+      'coverage-istanbul'
+    ],
+    coverageReporter: {
+      reporters:[
+          { 
+            type: "html", 
+            dir: "./", 
+            subdir: "coverage/html" ,
+            includeAllSources: true
+          }
+      ]
+    },
+    coverageIstanbulReporter: {
+      includeAllSources: true,
+      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: path.join(__dirname, 'coverage/istanbul'),
+      combineBrowserReports: true,
+      fixWebpackSourcePaths: true,
+      skipFilesWithNoCoverage: true,
+      'report-config': {
+        html: {
+          subdir: 'html'
+        }
+      },
+      thresholds: {
+        emitWarning: false,
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100
+        },
+        each: { // thresholds per file
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+          overrides: {
+            'src/**/*.ts': {
+              statements: 98
+            }
+          }
+        }
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
